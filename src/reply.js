14b59f9127ee543036f96e7acf25df8f5a6d7f86
callunaborealis <callunaborealis@gmail.com>
2021-01-27 01:22:51 +0800

    Fix interpret request

--- src/reply.js
+++ src/reply.js
@@ -1,9 +1,12 @@
+const isArray = require("lodash/isArray");
 const random = require("lodash/random");
 
 exports.interpretRequest = (message, listOfMatches) => {
   let matched = false;
   for (let i = 0; i < listOfMatches.length; i++) {
-    if (message.content.match(new RegExp(listOfMatches[i]))) {
+    const pattern = new RegExp(listOfMatches[i]);
+    const matches = message.content.match(pattern);
+    if (isArray(matches) && matches.length > 0) {
       matched = true;
       break;
     }
